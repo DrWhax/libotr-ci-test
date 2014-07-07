103c750e1c9917417386c60a55087ce8b87a5d62
jvoisin <julien.voisin@dustri.org>
2014-07-08 01:33:47 +0200

    Add more tests for proto.c

--- tests/unit/test_proto.c
+++ tests/unit/test_proto.c
@@ -1,7 +1,33 @@
 #include <tap/tap.h>
 #include <proto.h>
 
-#define NUM_TESTS 3
+#define NUM_TESTS 8
+
+static void test_otrl_proto_query_bestversion(void)
+{
+	const char *query2 = "?OTRv2?\n<b>alice</b> has requested an "
+		"<a href=\"https://otr.cypherpunks.ca/\">Off-the-Record "
+		"private conversation</a>.  However, you do not have a plugin "
+		"to support that.\nSee <a href=\"https://otr.cypherpunks.ca/\">"
+		"https://otr.cypherpunks.ca/</a> for more information.";
+
+	const char *query23 = "?OTRv23?\n<b>alice</b> has requested an "
+		"<a href=\"https://otr.cypherpunks.ca/\">Off-the-Record "
+		"private conversation</a>.  However, you do not have a plugin "
+		"to support that.\nSee <a href=\"https://otr.cypherpunks.ca/\">"
+		"https://otr.cypherpunks.ca/</a> for more information.";
+
+	const char *query3 = "?OTRv3?\n<b>alice</b> has requested an "
+		"<a href=\"https://otr.cypherpunks.ca/\">Off-the-Record "
+		"private conversation</a>.  However, you do not have a plugin "
+		"to support that.\nSee <a href=\"https://otr.cypherpunks.ca/\">"
+		"https://otr.cypherpunks.ca/</a> for more information.";
+
+	ok(otrl_proto_query_bestversion(query2, OTRL_POLICY_ALLOW_V2) == 2, "The best from query2 is 2");
+	ok(otrl_proto_query_bestversion(query3, OTRL_POLICY_ALLOW_V3) == 3, "The best from query3 is 3");
+	ok(otrl_proto_query_bestversion(query23, OTRL_POLICY_ALLOW_V2) == 2, "The best from query23 is 2");
+	ok(otrl_proto_query_bestversion(query23, OTRL_POLICY_ALLOW_V3) == 3, "The best from query23 is 3");
+}
 
 static void test_proto_default_query_msg(void)
 {
@@ -29,12 +55,14 @@ static void test_proto_default_query_msg(void)
 	ok(strcmp(expected2, msg2) == 0, "OTRv2 default query message is valid");
 	ok(strcmp(expected23, msg23) == 0, "OTRv23 default query message is valid");
 	ok(strcmp(expected3, msg3) == 0, "OTRv3 default query message is valid");
+	ok(otrl_proto_default_query_msg("alice", 0) == NULL, "Wrong version query message is NULL");
 }
 
 int main(int argc, char **argv)
 {
 	plan_tests(NUM_TESTS);
 	test_proto_default_query_msg();
+	test_otrl_proto_query_bestversion();
 	return 0;
 
 }
