e8a5d7ea94432ac38c505d16bc416d66d0b0e23b
jvoisin <julien.voisin@dustri.org>
2014-07-18 00:17:50 +0200

    Add some dh tests (and dos2unix; fuck you MS)

--- tests/unit/test_dh.c
+++ tests/unit/test_dh.c
@@ -5,8 +5,8 @@
  * under the terms of the GNU General Public License, version 2 only, as
  * published by the Free Software Foundation.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * This program is distributed in the hope that it will be useful, but WITHencrypt
+ * ANY WARRANTY; withencrypt even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
  * more details.
  *
@@ -19,7 +19,7 @@
 #include <gcrypt.h>
 #include <dh.h>
 
-#define NUM_TESTS 5
+#define NUM_TESTS 17
 
 /*
  * The re-implementation/inclusion of crypto stuff is
@@ -64,11 +64,134 @@ static void test_otrl_dh_gen_keypair(void)
 static void test_otrl_dh_keypair_free(void)
 {
 	DH_keypair kp;
-
 	otrl_dh_gen_keypair(DH1536_GROUP_ID, &kp);
 	otrl_dh_keypair_free(&kp);
 	ok(kp.pub == NULL && kp.priv == NULL && kp.groupid == DH1536_GROUP_ID,
 			"DH_keypair free'd with success");
+
+}
+
+static void test_otrl_dh_compute_v2_auth_keys(void)
+{
+	const char test_vector[] = "This is a test vector";
+    size_t slen = 0;
+	size_t sessionidlenp = 0;
+    unsigned char *sdata = NULL;
+    unsigned char *hashdata = NULL;
+    gcry_mpi_t s = NULL;
+    unsigned char ctr[16] = {0};
+
+	DH_keypair our_dh, their_dh;
+	gcry_mpi_t public_key = NULL;
+
+
+	unsigned char sessionid[8];
+	gcry_md_hd_t mac_m1 = NULL, mac_m1p = NULL, mac_m2 = NULL, mac_m2p = NULL;
+	gcry_cipher_hd_t enc_c = NULL, enc_cp = NULL;
+	unsigned char encrypt[32] = {0};
+
+	unsigned char sessionid_expected[8];
+	gcry_md_hd_t mac_m1_expected = NULL, mac_m1p_expected = NULL, mac_m2_expected = NULL, mac_m2p_expected = NULL;
+	gcry_cipher_hd_t enc_c_expected = NULL, enc_cp_expected = NULL;
+	unsigned char expected_encrypt[32] = {0};
+
+	otrl_dh_gen_keypair(DH1536_GROUP_ID, &our_dh);
+	otrl_dh_gen_keypair(DH1536_GROUP_ID, &their_dh);
+
+	our_dh.groupid++;
+	ok(otrl_dh_compute_v2_auth_keys(&our_dh, their_dh.pub,
+			sessionid, &sessionidlenp, &enc_c, &enc_cp,
+			&mac_m1, &mac_m1p, &mac_m2, &mac_m2p) == gcry_error(GPG_ERR_INV_VALUE),
+			"Invalid group detected");
+	our_dh.groupid--;
+	
+	gcry_mpi_scan(&public_key, GCRYMPI_FMT_USG, "1", 0, NULL);
+	ok(otrl_dh_compute_v2_auth_keys(&our_dh, public_key,
+			sessionid, &sessionidlenp, &enc_c, &enc_cp,
+			&mac_m1, &mac_m1p, &mac_m2, &mac_m2p) == gcry_error(GPG_ERR_INV_VALUE),
+			"Public key too small");
+
+    gcry_mpi_scan(&public_key, GCRYMPI_FMT_HEX, (const unsigned char *)DH1536_MODULUS_S, 0, NULL);
+    gcry_mpi_add_ui(public_key, DH1536_MODULUS, 1);
+	ok(otrl_dh_compute_v2_auth_keys(&our_dh, DH1536_MODULUS,
+			sessionid, &sessionidlenp, &enc_c, &enc_cp,
+			&mac_m1, &mac_m1p, &mac_m2, &mac_m2p) == gcry_error(GPG_ERR_INV_VALUE),
+			"Public key too big");
+
+	ok(otrl_dh_compute_v2_auth_keys(&our_dh, their_dh.pub, sessionid, &sessionidlenp, &enc_c, &enc_cp,
+			&mac_m1, &mac_m1p, &mac_m2, &mac_m2p) == gcry_error(GPG_ERR_NO_ERROR),
+			"Auth keys generated");
+	ok(sessionidlenp == 8, "Session id len p set to correct value");
+
+    s = gcry_mpi_snew(DH1536_MOD_LEN_BITS);
+    gcry_mpi_powm(s, their_dh.pub, our_dh.priv, DH1536_MODULUS);
+
+    gcry_mpi_print(GCRYMPI_FMT_USG, NULL, 0, &slen, s);
+    sdata = gcry_malloc_secure(slen + 5);
+    sdata[1] = (slen >> 24) & 0xff;
+    sdata[2] = (slen >> 16) & 0xff;
+    sdata[3] = (slen >> 8) & 0xff;
+    sdata[4] = slen & 0xff;
+    gcry_mpi_print(GCRYMPI_FMT_USG, sdata+5, slen, NULL, s);
+    gcry_mpi_release(s);
+
+    hashdata = gcry_malloc_secure(32);
+    sdata[0] = 0x00;
+    gcry_md_hash_buffer(GCRY_MD_SHA256, hashdata, sdata, slen+5);
+    memmove(sessionid_expected, hashdata, 8);
+	ok(memcmp(sessionid_expected, sessionid, 8) == 0, "Session id is correct");
+
+    sdata[0] = 0x01;
+    gcry_md_hash_buffer(GCRY_MD_SHA256, hashdata, sdata, slen+5);
+
+    gcry_cipher_open(&enc_c_expected, GCRY_CIPHER_AES, GCRY_CIPHER_MODE_CTR, GCRY_CIPHER_SECURE);
+    gcry_cipher_setkey(enc_c_expected, hashdata, 16);
+    gcry_cipher_setctr(enc_c_expected, ctr, 16);
+	gcry_cipher_encrypt(enc_c_expected, expected_encrypt, sizeof(expected_encrypt), test_vector, strlen(test_vector));
+	gcry_cipher_encrypt(enc_c, encrypt, sizeof(encrypt), test_vector, strlen(test_vector));
+	ok(memcmp(encrypt, expected_encrypt, sizeof(encrypt)) == 0, "Enc ok");
+
+    gcry_cipher_open(&(enc_cp_expected), GCRY_CIPHER_AES, GCRY_CIPHER_MODE_CTR, GCRY_CIPHER_SECURE);
+    gcry_cipher_setkey(enc_cp_expected, hashdata+16, 16);
+    gcry_cipher_setctr(enc_cp_expected, ctr, 16);
+	gcry_cipher_encrypt(enc_cp_expected, expected_encrypt, sizeof(expected_encrypt), test_vector, strlen(test_vector));
+	gcry_cipher_encrypt(enc_cp, encrypt, sizeof(encrypt), test_vector, strlen(test_vector));
+	ok(memcmp(encrypt, expected_encrypt, sizeof(encrypt)) == 0, "Encp ok");
+
+    sdata[0] = 0x02;
+    gcry_md_hash_buffer(GCRY_MD_SHA256, hashdata, sdata, slen+5);
+    gcry_md_open(&mac_m1_expected, GCRY_MD_SHA256, GCRY_MD_FLAG_HMAC);
+    gcry_md_setkey(mac_m1_expected, hashdata, 32);
+	gcry_md_write(mac_m1_expected, test_vector, sizeof(test_vector));
+	gcry_md_write(mac_m1, test_vector, sizeof(test_vector));
+	ok(memcmp(gcry_md_read(mac_m1_expected, 0), gcry_md_read(mac_m1, 0), 32) == 0, "mac_m1 set");
+
+    sdata[0] = 0x03;
+    gcry_md_hash_buffer(GCRY_MD_SHA256, hashdata, sdata, slen+5);
+    gcry_md_open(&mac_m2_expected, GCRY_MD_SHA256, GCRY_MD_FLAG_HMAC);
+    gcry_md_setkey(mac_m2_expected, hashdata, 32);
+	gcry_md_write(mac_m2_expected, test_vector, sizeof(test_vector));
+	gcry_md_write(mac_m2, test_vector, sizeof(test_vector));
+	ok(memcmp(gcry_md_read(mac_m2_expected, 0), gcry_md_read(mac_m2, 0), 32) == 0, "mac_m2 set");
+
+    sdata[0] = 0x04;
+    gcry_md_hash_buffer(GCRY_MD_SHA256, hashdata, sdata, slen+5);
+    gcry_md_open(&mac_m1p_expected, GCRY_MD_SHA256, GCRY_MD_FLAG_HMAC);
+    gcry_md_setkey(mac_m1p_expected, hashdata, 32);
+	gcry_md_write(mac_m1p_expected, test_vector, sizeof(test_vector));
+	gcry_md_write(mac_m1p, test_vector, sizeof(test_vector));
+	ok(memcmp(gcry_md_read(mac_m1p_expected, 0), gcry_md_read(mac_m1p, 0), 32) == 0, "mac_m1p set");
+
+    sdata[0] = 0x05;
+    gcry_md_hash_buffer(GCRY_MD_SHA256, hashdata, sdata, slen+5);
+    gcry_md_open(&mac_m2p_expected, GCRY_MD_SHA256, GCRY_MD_FLAG_HMAC);
+    gcry_md_setkey(mac_m2p_expected, hashdata, 32);
+	gcry_md_write(mac_m2p_expected, test_vector, sizeof(test_vector));
+	gcry_md_write(mac_m2p, test_vector, sizeof(test_vector));
+	ok(memcmp(gcry_md_read(mac_m2p_expected, 0), gcry_md_read(mac_m2p, 0), 32) == 0, "mac_m2p set");
+
+    gcry_free(sdata);
+    gcry_free(hashdata);
 }
 
 int main(int argc, char **argv)
@@ -86,5 +209,6 @@ int main(int argc, char **argv)
 
 	test_otrl_dh_gen_keypair();
 	test_otrl_dh_keypair_free();
+	test_otrl_dh_compute_v2_auth_keys();
 	return 0;
 }
